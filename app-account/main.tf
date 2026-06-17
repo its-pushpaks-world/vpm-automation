76eb6523285857dee9d3eee582e0edc42c0a0673
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2026-06-17 20:45:38 +0530

    task(app-account): create config

---
+++ app-account/main.tf
@@ -0,0 +1,30 @@
+# 1. The default provider (Deploys the EC2 instance here)
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# 2. The aliased provider (Assumes the role in the Platform account)
+provider "aws" {
+  alias  = "platform_reader"
+  region = "ap-south-1"
+  assume_role {
+    # The ARN outputted from Part 1
+    role_arn = "arn:aws:iam::242655703609:role/CrossAccountAMIReaderRole"
+  }
+}
+
+# 3. Call your module, passing the aliased provider
+module "frontend_instance" {
+  source = "./../Assignment/modules/ec2-instance"
+
+  name              = "frontend-app"
+  environment       = "prod"
+  instance_type     = "t3.micro"
+  ami_ssm_parameter = "/platform/amis/frontend/latest"
+
+  # Pass the providers explicitly!
+  providers = {
+    aws          = aws
+    aws.platform = aws.platform_reader
+  }
+}

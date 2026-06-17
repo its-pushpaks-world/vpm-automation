6e779f60fb98b716352fc25b999cde294a9b2cb4
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2026-06-17 23:30:39 +0530

    task(Simulated Drift Detection Logic): update output.tf

--- modules/ec2-instance/outputs.tf
+++ modules/ec2-instance/outputs.tf
@@ -1,11 +1,21 @@
 # Output declaration
 
-output "instance_id" {
-  value       = aws_instance.ec2-tf.id
-  description = "EC2 instance id"
+output "instance_ids" {
+  value       = data.aws_instances.asg_instances.ids
+  description = "List of EC2 instance IDs currently running in the ASG"
 }
 
-output "private_ip" {
-  value       = aws_instance.ec2-tf.private_ip
-  description = "EC2 instance private IP address"
+output "private_ips" {
+  value       = data.aws_instances.asg_instances.private_ips
+  description = "List of private IP addresses for the instances currently running in the ASG"
+}
+
+output "asg_name" {
+  value       = aws_autoscaling_group.ec2_asg.name
+  description = "The name of the Auto Scaling Group"
+}
+
+output "launch_template_latest_version" {
+  value       = aws_launch_template.ec2_lt.latest_version
+  description = "The latest generated version of the Launch Template"
 }

3ae60e9ccebf355a0797e384dfed5a788e9a87fb
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2026-06-21 20:16:50 +0530

    fix(app-account): correct main.tf

--- app-account/main.tf
+++ app-account/main.tf
@@ -1,6 +1,22 @@
+terraform {
+  # The remote backend destination for App Account
+  backend "s3" {
+    bucket       = "vpm-app-tfstate-prod" # Choose a unique name
+    key          = "app/terraform.tfstate"
+    region       = "ap-south-1"
+    use_lockfile = true
+  }
+}
+
 # 1. The default provider (Deploys the EC2 instance here)
 provider "aws" {
   region = "ap-south-1"
+}
+
+# Call the shared module to manage the app state bucket local to this account
+module "state_backend" {
+  source      = "../modules/remote-backend"
+  bucket_name = "vpm-app-tfstate-prod"
 }
 
 # 2. The aliased provider (Assumes the role in the Platform account)

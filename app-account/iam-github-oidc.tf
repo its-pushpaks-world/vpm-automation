f67df0d5030b9126d0e2cd6a7a9b3d00da700a8f
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2026-06-21 20:45:58 +0530

    task(app-account): create iam-github-oidc.tf

---
+++ app-account/iam-github-oidc.tf
@@ -0,0 +1,117 @@
+# -------------------------------------------------------------------------
+# 1. Fetch the EXISTING GitHub OIDC Provider
+# -------------------------------------------------------------------------
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+# -------------------------------------------------------------------------
+# 2. The IAM Role and Trust Policy
+# -------------------------------------------------------------------------
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "github_actions_terraform_role" {
+  name = "GitHubActionsTerraformRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = data.aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:its-pushpaks-world/*:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# -------------------------------------------------------------------------
+# 3. The Permissions Policy for Terraform
+# -------------------------------------------------------------------------
+resource "aws_iam_role_policy" "terraform_permissions" {
+  name = "TerraformDeploymentPermissions"
+  role = aws_iam_role.github_actions_terraform_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid": "CrossAccountPlatformRoleAssumption",
+        "Effect": "Allow",
+        "Action": "sts:AssumeRole",
+        # Explicitly locked down to your Platform Account ID for best practice security
+        "Resource": "arn:aws:iam::242655703609:role/CrossAccountAMIReaderRole"
+      },
+      {
+        "Sid": "TerraformS3RemoteStateAndLocking",
+        "Effect": "Allow",
+        "Action": [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:CreateBucket",
+          "s3:PutBucketVersioning",
+          "s3:PutEncryptionConfiguration",
+          "s3:PutBucketPublicAccessBlock"
+        ],
+        "Resource": [
+          "arn:aws:s3:::vpm-app-tfstate-prod",
+          "arn:aws:s3:::vpm-app-tfstate-prod/*"
+        ]
+      },
+      {
+        "Sid": "AutoScalingAndSchedulingOperations",
+        "Effect": "Allow",
+        "Action": [
+          "autoscaling:CreateAutoScalingGroup",
+          "autoscaling:UpdateAutoScalingGroup",
+          "autoscaling:DeleteAutoScalingGroup",
+          "autoscaling:DescribeAutoScalingGroups",
+          "autoscaling:DescribeAutoScalingInstances",
+          "autoscaling:DescribeScalingActivities",
+          "autoscaling:PutScheduledUpdateGroupAction",
+          "autoscaling:DeleteScheduledUpdateGroupAction",
+          "autoscaling:DescribeScheduledActions"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "LaunchTemplateManagement",
+        "Effect": "Allow",
+        "Action": [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateLaunchTemplateVersion",
+          "ec2:DeleteLaunchTemplate",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "ec2:DeleteTags"
+        ],
+        "Resource": "*"
+      },
+      {
+        "Sid": "NetworkAndInfrastructureDiscovery",
+        "Effect": "Allow",
+        "Action": [
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeInstances"
+        ],
+        "Resource": "*"
+      }
+    ]
+  })
+}

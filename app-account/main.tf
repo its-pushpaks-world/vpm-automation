23ffb6d1facb351c99df6b0954c49fb700254068
its-pushpaks-world <115705491+its-pushpaks-world@users.noreply.github.com>
2026-06-21 22:30:28 +0530

    task(app-account): update main.tf

--- app-account/main.tf
+++ app-account/main.tf
@@ -1,31 +1,27 @@
 terraform {
-  # The remote backend destination for App Account
   backend "s3" {
-    bucket       = "vpm-app-tfstate-prod" # Choose a unique name
+    bucket       = "vpm-app-tfstate-prod"
     key          = "app/terraform.tfstate"
     region       = "ap-south-1"
     use_lockfile = true
   }
 }
 
-# 1. The default provider (Deploys the EC2 instance here)
 provider "aws" {
   region = "ap-south-1"
 }
 
-# Call the shared module to manage the app state bucket local to this account
 module "state_backend" {
   source      = "../modules/remote-backend"
   bucket_name = "vpm-app-tfstate-prod"
 }
 
-# 2. The aliased provider (Assumes the role in the Platform account)
+# 2. Dynamic Aliased Provider pointing directly to your variable block
 provider "aws" {
   alias  = "platform_reader"
   region = "ap-south-1"
   assume_role {
-    # The ARN outputted from Part 1
-    role_arn = "arn:aws:iam::242655703609:role/CrossAccountAMIReaderRole"
+    role_arn = "arn:aws:iam::${var.platform_account_id}:role/CrossAccountAMIReaderRole"
   }
 }
 
@@ -34,7 +30,7 @@ data "aws_ssm_parameter" "frontend_ami" {
   name     = "/platform/amis/frontend/latest"
 }
 
-# 3. Call your module, passing the aliased provider
+# 3. Call your module
 module "frontend_instance" {
   source = "./../modules/ec2-instance"
 
@@ -43,7 +39,6 @@ module "frontend_instance" {
   instance_type = "t3.micro"
   ami_id        = data.aws_ssm_parameter.frontend_ami.value
 
-  # Pass the providers explicitly!
   providers = {
     aws          = aws
     aws.platform = aws.platform_reader
